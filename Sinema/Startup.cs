9d2d4bf93ecb20e9d03bd35e467eae6c06e1a98b
Tyrone McGhee <tmcghee1970@gmail.com>
2020-06-17 21:29:35 -0400

    Add project files.

---
+++ Sinema/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sinema.Startup))]
+namespace Sinema
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
